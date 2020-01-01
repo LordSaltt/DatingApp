dfaab8cfc1b14ccd0c490495a4c605a997b68b55
Josue Salinas <saltt17@gmail.com>
2019-12-31 18:06:44 -0600

    Adding routing on client

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/Alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -24,7 +26,7 @@ export class NavComponent implements OnInit {
       },
       error => {
         this.alertify.error(error);
-      }
+      }, () => { this.router.navigate(['/members']); }
     );
   }
 
@@ -35,5 +37,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.warning('Logged out!');
+    this.router.navigate(['/home']);
   }
 }
